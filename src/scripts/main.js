e207c7ec96a4035d7ba37c7cbc8615649c3d9aa7
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-12-11 22:17:35 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+// eslint-disable-next-line no-new
+new Promise((resolve) => {
+  resolve(
+    logo.insertAdjacentHTML(
+      'beforeend',
+      '<div class="message">Promise was resolved!</>',
+    ),
+  );
+});
+
+// eslint-disable-next-line promise/param-names, no-new
+new Promise((regect) => {
+  setTimeout(() => {
+    regect(
+      logo.insertAdjacentHTML(
+        'beforeend',
+        '<div class="message error-message">Promise was rejected!</>',
+      ),
+    );
+  }, 3000);
+});
